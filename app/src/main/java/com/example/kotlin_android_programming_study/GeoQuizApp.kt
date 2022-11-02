b01a71440953d2aad8e7fd26407b830c528a6a43
kwonjuyoung <20161527@vision.hoseo.edu>
2022-11-02 23:57:02 +0900

    4장 : UI상태유지

--- app/src/main/java/com/example/kotlin_android_programming_study/GeoQuizApp.kt
+++ app/src/main/java/com/example/kotlin_android_programming_study/GeoQuizApp.kt
@@ -17,9 +17,7 @@ private const val TAG = "Activity"
 
 class GeoQuizApp : AppCompatActivity() {
 
-    //추가 알고있던 것 메모 : lateinit은 늦은 초기화로 말 그대로 선언만 하고 초기화를 나중에 해줄 수 있다.
-    //또한 var로 선언했을 경우에만 사용이 가능하다.
-    //null 허용 프로퍼티나 기초 타입 프로퍼티에는 사용할 수 없다.
+    //ㅋ
     private lateinit var trueButton : Button
     private lateinit var falseButton : Button
     private lateinit var nextButton: ImageButton
